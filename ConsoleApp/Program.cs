7d312f536917f6fa8a6cc10fb7e47cfb45b21696
Ryan-Lei <ryan.ktl@gmail.com>
2021-06-30 21:11:13 +0800

    implement IsFileExist method.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MiniFtp miniFtp = new MiniFtp("ftpuser", "ftpuser$");
+            Uri uri = new Uri("ftp://192.168.100.118/vmware_workstation_key.txt");
+            miniFtp.IsFileExist(uri);
+
+        }
+    }
+}
---
+++ miniFtp/MiniFtp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+public class MiniFtp
+{ 
+
+    public MiniFtp(string userName,string password)
+    {
+        FtpNetworkCredential = new NetworkCredential();
+        FtpNetworkCredential.UserName = userName;
+        FtpNetworkCredential.Password = password;
+    }
+
+    public MiniFtp(NetworkCredential networkCredential)
+    {
+        FtpNetworkCredential = networkCredential;
+    }
+
+    public  bool IsFileExist(Uri uri)
+    {
+        var request = (FtpWebRequest)WebRequest.Create(uri);
+        request.Credentials = FtpNetworkCredential;
+        request.Method = WebRequestMethods.Ftp.GetFileSize;
+        bool isExist = false;
+
+        try
+        {
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+            isExist= true;
+        }
+        catch (WebException webException)
+        {
+            FtpWebResponse response = (FtpWebResponse)webException.Response;
+
+            if (response.StatusCode == FtpStatusCode.ActionNotTakenFileUnavailable)
+            {
+                isExist = false;
+            } 
+            else
+            {
+                throw webException;
+            }  
+        }
+        return isExist;
+    }
+
+    public NetworkCredential FtpNetworkCredential { get; set; } 
+
+}
