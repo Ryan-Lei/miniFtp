d78d748fb37f9b3f56612c266182b7473ca6b605
Ryan-Lei <ryan.ktl@gmail.com>
2021-07-01 17:07:20 +0800

    write miniFtp example in ConsoleApp.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -11,8 +11,40 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
- 
-            Console.WriteLine("press enter to finish !!");
+            MiniFtp miniFtp = new MiniFtp("UserName", "Password");
+            Uri uri = new Uri("ftp://host/dir/temp.txt");
+
+            Console.WriteLine("IsFileExist: "+miniFtp.IsFileExist(uri));
+
+            uri = new Uri("ftp://host/dir");
+            Console.WriteLine("IsDirectoryExist: " + miniFtp.IsDirectoryExist(uri));
+
+            //uri = new Uri("ftp://host/dir");
+            //List<FolderFile> folderFiles = miniFtp.GetFileList(uri);
+
+            //foreach(var folderFile in folderFiles)
+            //{
+            //    Console.WriteLine(folderFile.FilePath);
+            //}
+
+            //uri = new Uri("ftp://host/dir/temp.txt");
+            //miniFtp.DownloadFile(uri, @"c:\temp\temp.txt");
+            //Console.WriteLine("File Downloaded!! ");
+
+            //uri = new Uri("ftp://host/dir/temp.txt");
+            //miniFtp.UploadFile(uri, @"c:\temp\temp.txt");
+            //Console.WriteLine("File Uploaded!! ");
+
+            //uri = new Uri("ftp://host/dir/temp.txt");
+            //miniFtp.DeleteFile(uri);
+            //Console.WriteLine("File Deleted!! ");
+
+            //uri = new Uri("ftp://host/dir/temp.txt");
+            //miniFtp.RenameFile(uri,"newFile.txt");
+            //Console.WriteLine("File Renamed!! ");
+
+            Console.WriteLine();
+            Console.WriteLine("Press Enter to Finish !!");
             Console.ReadLine();
 
         }
